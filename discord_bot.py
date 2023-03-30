ce607c351b32a22150e7ea07617f4c74dbaa05d8
MatFreitas <matheusfs2@al.insper.edu.br>
2023-03-30 12:52:45 -0300

    APS parte 2

--- discord_bot.py
+++ discord_bot.py
@@ -1,25 +1,93 @@
 import os
 import re
+import json
 import discord
 import requests
+import stopwords
+from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from nltk.corpus import wordnet
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# -------------------------------------------------------------------
+# Setup
 
 load_dotenv()
 
-client = discord.Client()
+# Setting Client
 intents = discord.Intents.default()
 intents.members = True
-
 client = discord.Client(intents=intents)
+
+# Dictionary to store titles and texts of resulting scraped pages
+scrap = {}
+scrap['title'] = []
+scrap['texts'] = []
+
+# Inverted index dicitonary
+indice = {}
+
+# List of urls, initialized with a random url
+# urls = ["https://www.naughtydog.com/company"]
+
+# Headers to mimic a browser visit
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+# Vectorizer to calculate TFIDF measure
+vectorizer = TfidfVectorizer()
+
+# Control variables
+total_urls = 0
+first_url = True
+ # -------------------------------------------------------------------
+
+# -------------------------------------------------------------------
+# Seach functions -
+def buscar(palavras, indice):
+    # Dictionary to organize search results according to highest TFIDF
+    assert type(palavras)==list
+    resultado = dict()
+    for p in palavras:
+        if p in indice.keys():
+            for documento in indice[p].keys():
+                if documento not in resultado.keys():
+                    resultado[documento] = indice[p][documento]
+                else:
+                    resultado[documento] += indice[p][documento]
+    return resultado
+
+def n_maiores(res_busca, n):
+    # Sorting result to most relevant to least relevant (in casy n>1)
+    res = []
+    for k in res_busca.keys():
+        # Relevância e documento
+        res.append([res_busca[k], k])
+    res = sorted(res, reverse = True)[0:n]
+    return res
+
+def query(query_string, n, indice):
+    palavras = re.findall('\w+', query_string)
+    res = buscar(palavras, indice)
+    res_n = n_maiores(res, n)
+    return res_n
+# -------------------------------------------------------------------
 
 @client.event
 async def on_ready():
     guild = discord.utils.get(client.guilds, name=os.getenv('GUILD_NAME'))
     channel = discord.utils.get(guild.text_channels, name=os.getenv('CHANNEL_NAME'))
+
     # await channel.send('O bot está online!')
 
 @client.event
 async def on_message(message):
+    global scrap
+    global indice
+    global headers
+    global first_url
+    global vectorizer
+    global total_urls
+
     if message.author == client.user:
         return
 
@@ -34,7 +102,7 @@ async def on_message(message):
                                    "!run <country> currency: returns country's currency.\n"\
                                    "!run <country> capital: returns country's capital.\n"\
                                    "!run <country> flag: returns country's flag description.\n"\
-                                   "If country name is a compound name, you can write it partially."\
+                                   "If country name is a compound name, you can write it partially.\n"\
                                    "Info obtained at: https://restcountries.com/")
 
     if message.content.lower()[:4] == '!run':
@@ -73,5 +141,94 @@ async def on_message(message):
 
         else:
             await message.channel.send(f"'{args[1]}' is not a valid option! Try using '!help' to understand how to use '!run'.")     
+
+    if message.content.lower()[:6] == '!crawl':
+        # Update control total urls variable
+        total_urls += 1
+
+        # Getting url from arg
+        url = message.content.lower().split()[1]
+
+        # Returns a requests.models.Response object
+        try:
+            res = requests.get(url, headers=headers)
+            html_page = res.content
+        except:
+            await message.channel.send(f"'{url}' does not seem to be a valid URL!")     
+            return
+
+        # Getting title and text of page
+        soup = BeautifulSoup(html_page, 'html.parser')
+        text_list = soup.find_all('p')
+        title_list = soup.find('title')
+
+        # Adding title and text of HTML to scraped pages dictionary
+        scrap['title'].append(title_list.text)
+        scrap['texts'].append(" ".join([text_list[i].text for i in range(len(text_list))]).replace("\n", " "))
+
+        # Updating TFIDF vectorizer
+        tfidf = vectorizer.fit_transform(scrap['texts'])
+
+        # Updating inverted index dictionary
+        new_doc_idx = total_urls - 1
+        new_doc_name = scrap['title'][-1]
+        for word, idx in vectorizer.vocabulary_.items():
+            if word in scrap['texts'][-1].split():
+                # If word wasn't in any previous document, create its instance
+                if word not in indice.keys():
+                    indice[word] = {}
+
+                # Update tfidf of word in new document
+                indice[word][new_doc_name] = tfidf[new_doc_idx, idx]
+
+    if message.content.lower()[:7] == '!search':
+        res = query(message.content.lower()[8:], 1, indice)
+
+        # Cheking if there were results to the query search
+        if res != []:
+            await message.channel.send("Search result:\n"\
+                                    f"{res[0][1]}")
+        else:
+            await message.channel.send("No results found!")
+
+    if message.content.lower()[:10] == '!wn_search':
+        # Getting terms
+        terms =re.findall('\w+', message.content.lower()[10:])
+
+        # Creating dic to store most similar terms to the ones searched
+        relevancy = {}
+
+        # To every term in vocabylary, find the relevancy 
+        # it has to searched words
+        for voc in vectorizer.vocabulary_:
+            valor_soma = 0
+            for word in terms:
+                # Check if vocabulary term is not a stop word
+                if voc.lower() not in stopwords.stop_words:
+                    syns1 = wordnet.synsets(word)
+                    syns2 = wordnet.synsets(voc)
+                    # Check if there exists word in wordnet vocabulary
+                    if len(syns1) > 0 and len(syns2) > 0:
+                        # Score of similarity between words
+                        similarity = syns1[0].wup_similarity(syns2[0])
+                        if similarity is not None:
+                            valor_soma += similarity
+            relevancy[voc] = valor_soma
+
+        # Sort dic and get the 5 most similar words
+        similar_terms = [i[0] for i in sorted(relevancy.items(), key=lambda x:x[1], reverse=True)[:5]]
+
+        # Formatting query string
+        query_string = ' '.join(similar_terms)
+
+        # Searching term in database
+        res = query(query_string, 1, indice)
+
+        # Cheking if there were results to the query search
+        if res != []:
+            await message.channel.send("Wordnet Search result:\n"\
+                                    f"{res[0][1]}")
+        else:
+            await message.channel.send("No results found!")
 
 client.run(os.getenv('TOKEN'))
---
+++ ensaio_2.md
@@ -0,0 +1,19 @@
+## Como foi o plano? 
+
+Para realizar essa parte da APS, primeiramente estava pensando em calcular os TFIDFs manualmente. No entanto, isso se provou uma tarefa muito
+duradoura, portanto mudei a estratégia para utilizar o vectorizer o que facilitou as tarefas. Reutilizei grande parte dos códigos feitos em 
+aula, apenas adaptando eles para o contexto da APS.
+
+## Que dificuldades foram encontradas?
+
+Tive dificuldade em entender o que era "base de dados". Por ficar meio aberto, entendi que precisava realmente de alguma tecnologia como MySQL,
+no entanto, no final, foi apenas necessário usar variáveis globais entre as funções assíncronas.
+
+## Quais foram as fontes consultadas?
+
+Aulas do curso & ChatGPT.
+
+## Interações com o GPT
+
+- [Como criar índice invertido](https://chat.openai.com/chat/3a2c33d7-42d0-456d-9075-095951b8d5e0)
+- [Utilizando Beautiful Soup](https://chat.openai.com/chat/247fcfeb-b06b-4924-adf2-b342d4b13b3c)
